4f82544be1af668a9dc9c9333945e6b3e86bc2f6
VirginiaCreativa <virginiavelasquezsoto@gmail.com>
2018-07-15 19:33:28 -0500

    Listo maqueta CONJUGACION

--- src/components/pages/conjugacion/conjugacion_item.jsx
+++ src/components/pages/conjugacion/conjugacion_item.jsx
@@ -1,36 +1,26 @@
 import React, { Component } from 'react';
-import PropTypes from"prop-types";
 
-class PalabraSemana extends Component {
+class Conjugacion_Item extends Component {
    render() {
 
-      const { title, abrev, definition, imageSrc, topImage } = this.props;
-      const sizeTopImage = {
-        top: topImage + 'px'
-      }
+      const { tema, yo, tu, persona, nosotros, vosotros, ellos } = this.props;
 
       return (
-      <div className='palabrasemana'>
-      	<a href='' onMouseOver={this.handleClick} >
-          <div className="imagen">
-            <div className='img-hover'><i className="fas fa-play"></i></div>
-            <img src={imageSrc} className="img-fluid" alt={title} style={sizeTopImage}/>
-          </div>
-          <article>
-            <h6>{title}</h6>
-            <p><span className="abrev">{abrev}.</span> {definition}</p>
-          </article>
-      	</a>
+      <div className='conjugacion'>
+      	<div className="item_verbo">
+          <h3>{tema}</h3>
+          <div className="item_verbo">
+          <p className="graytxt">{yo}</p>
+          <p className="graytxt">{tu}</p>
+          <p className="graytxt">{persona}</p>
+          <p className="graytxt">{nosotros}</p>
+          <p className="graytxt">{vosotros}</p>
+          <p className="graytxt">{ellos}</p>
+        </div>
+        </div>
       </div>
       );
    }
 }
 
-PalabraSemana.propTypes = {
-  title: PropTypes.string,
-  definition: PropTypes.string,
-  imageSrc: PropTypes.string,
-  abrev: PropTypes.oneOf(['tr', 'adj', 'deter', 'f', 'm', 'prnl', 'pron', 'subj', 'prep', 'irreg', 'intr', 'tr y prnl'])
-};
-
-export default PalabraSemana;
+export default Conjugacion_Item;
--- src/components/pages/conjugacion/conjugacion_stage.jsx
+++ src/components/pages/conjugacion/conjugacion_stage.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import Conjugacion_Item from './conjugacion_item.jsx'
+import './conjugacion.scss'
 
 export class Conjugacion_Stage extends Component {
   render() {
   
-  const { description, title, palabras} = this.props.data;
+  const { description, palabra, title, verbos} = this.props.data;
 
   return (
     <div>
@@ -12,12 +13,23 @@ export class Conjugacion_Stage extends Component {
         <h6 >{description}</h6>
         <h2>{title}</h2>
       </div>
-      <div className="box_stage_palabrasemana">
-       {/* {
-          palabras.map((item, id) => {
+      <div className="item_palabra">
+        <h4>{palabra}</h4>
+      </div>
+      <div className="box_stage_conjugacion">
+        <div className="item_verbo">
+          <p className="graytxt">Yo</p>
+          <p className="graytxt">Tú</p>
+          <p className="graytxt">Él/Ella/Usted</p>
+          <p className="graytxt">Nosotros</p>
+          <p className="graytxt">Vosotros</p>
+          <p className="graytxt">Ellos/Ellas/Ustedes</p>
+        </div>
+        {
+          verbos.map((item, id) => {
             return <Conjugacion_Item key={item.id} {...item}/>
           })
-        }*/}
+        }
       </div>
     </div>
   );
